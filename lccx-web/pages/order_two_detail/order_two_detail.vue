4cee95a5a8afcddc36fdb45b32c450c5f83c3406
guzhenfu <867124018@mslife.com>
2019-03-26 17:57:42 +0800

    add feedback

---
+++ lccx-web/pages/order_two_detail/order_two_detail.vue
@@ -0,0 +1,360 @@
+<template>
+	<div class="contain">
+		<div class="title_wrap">
+			交强险
+		</div>
+		<div class="header uni-between-item">
+			
+			<div class="brand_wrap">
+				<img src="../../static/img/baoxian_icon.png" >
+				<span>大地车险</span>
+			</div>
+			
+			
+			
+		</div>
+		
+		<div class="content_wrap">
+			<div class="content_item uni-between-item">
+				<span>投保地区</span>
+				<div>{{item.quote_details.driver_seat}}万</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>保障期限</span>
+				<div>{{item.quote_details.driver_seat}}万</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>保单号：</span>
+				<div>{{item.quote_details.driver_seat}}万</div>
+			</div>
+		</div>
+		
+		
+		<div class="title uni-between-item">
+			<span>交强险</span>
+		</div>
+		
+		<div class="content_wrap">
+			
+			<div class="content_item uni-between-item">
+				<span>交强险</span>
+				<div>{{item.quote_details.driver_seat}}万</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>车船税</span>
+				<div>{{item.quote_details.passenger_seat}}万</div>
+			</div>
+			
+			
+		</div>
+		
+		
+		<div class="title uni-between-item">
+			<span>车主信息</span>
+		</div>
+		
+		<div class="content_wrap">
+			<div class="content_item uni-between-item">
+				<span>姓名</span>
+				
+				<div>{{item.name}}</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>手机号</span>
+				
+				<div>{{item.trueMobile}}</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>身份证号</span>
+				<div>{{item.trueIdcard}}</div>
+			</div>
+		</div>
+		
+		<div class="title uni-between-item">
+			<span>车辆信息</span>
+		</div>
+		
+		<div class="content_wrap" style=" padding-bottom: 20upx;">
+			<div class="content_item uni-between-item">
+				<span>车牌号码</span>
+				
+				<div>{{item.license_no}}</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>品牌型号</span>
+				
+				<div>{{item.brand}}</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>车辆识别代码</span>
+				<div>{{item.trueVin}}</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>发动机号码</span>
+				
+				<div>{{item.trueEngineNo}}</div>
+			</div>
+			
+			<div class="content_item uni-between-item">
+				<span>注册日期</span>
+				<div>{{item.first_reg_date}}</div>
+			</div>
+			
+		</div>
+
+
+		  <div class=" button" @click="applyUnderwrite">我要理赔</div>
+  
+		
+	</div>
+</template>
+
+<script>
+	import { BASE_IMAGE_URL,getQuotations,applyUnderwrite } from "@/utils/api";
+	
+	export default {
+	  data () {
+		return {
+		  global: {},
+		  item: {
+			  quote_details: {}
+		  },
+		  quote_result: {}
+		}
+	  },
+
+	  methods: {
+		next: function(){
+		  const url = '../pay_address/main';
+		  uni.navigateTo({ url })
+		},
+
+		async applyUnderwrite(){
+		  let params = {
+			"insurer": this.item.quote_result.data[0].insurerCode,
+			"biz_id": this.item.biz_id,
+			"channel_code": "QUANLIAN_PROXY_INSURE",
+			"address_name": this.global.name,
+			"address_mobile": this.global.mobile,
+			"address_details": "路",
+			"address_county": "110112",
+			"address_city": "110100",
+			"address_province": "110000",
+			"policy_email": "caingougou@qq.com"
+		  };
+
+		  let res = await applyUnderwrite(params);
+		  if(res.code == 200){
+			this.global.monthly_expense = this.item.monthly_expense;
+			this.global.mileage_expense = this.item.mileage_expense;
+			this.global.compulsory = this.item.compulsory;
+			this.global.tax = this.item.tax;
+			uni.setStorageSync('global', this.global);
+			this.next();
+		  }
+		},
+
+		async getQuotations(){
+		  let res = await getQuotations(this.global.quotation_id);
+		  if(res.code == 200){
+			  console.log(res.data);
+			this.item = res.data;
+			this.item.quote_details = JSON.parse(this.item.quote_details);
+			if(parseInt(this.item.quote_details.liability) > 1000){
+			  this.item.quote_details.liability = parseInt(this.item.quote_details.liability/10000);
+			}
+
+			if(parseInt(this.item.quote_details.driver_seat) > 1000){
+			  this.item.quote_details.driver_seat = parseInt(this.item.quote_details.driver_seat/10000);
+			}
+
+			if(parseInt(this.item.quote_details.passenger_seat) > 1000){
+			  this.item.quote_details.passenger_seat = parseInt(this.item.quote_details.passenger_seat/10000);
+			}
+
+			if(this.item.mobile){
+			  this.item.trueMobile = this.item.mobile.substring(0,3)+"****"+this.item.mobile.substring(7,11);
+			}
+
+			if(this.item.idcard){
+			  this.item.trueIdcard = this.item.idcard.substring(0,3)+"***********"+this.item.idcard.substring(14,18);
+			}
+
+			if(this.item.vin){
+			  this.item.trueVin = this.item.vin.substring(0,3)+"***********"+this.item.vin.substring(this.item.vin.length-3,this.item.vin.length);
+			}
+
+			if(this.item.engine_no){
+			  this.item.trueEngineNo = this.item.engine_no.substring(0,1)+"****"+this.item.engine_no.substring(this.item.engine_no.length-1,this.item.engine_no.length);
+			}
+
+			this.item.quote_details.excluding = this.item.quote_details.excluding == "true";
+
+			console.log(this.item);
+			this.global.biz_id = this.item.biz_id;
+			this.item.quote_result = JSON.parse(this.item.quote_result);
+			this.$forceUpdate();
+		  }
+		}
+
+	  },
+
+	  onLoad(){
+		this.global = uni.getStorageSync("global");
+		this.getQuotations();
+	  }
+
+	}
+</script>
+
+<style>
+	.contain{
+		width: 100%;
+	}
+	.title_wrap{
+		width: 100%;
+		height: 94upx;
+		line-height: 94upx;
+		text-align: center;
+		color: #070707;
+		font-size: 34upx;
+		border-bottom: 1px solid rgba(0,0,0,0.1);
+	}
+	.header{
+		margin-top: 60upx;
+		width: 100%;
+		padding: 0upx 40upx;
+		box-sizing: border-box;
+	}
+	.brand_wrap{
+		display: flex;
+		flex-direction: column;
+	}
+	
+	.brand_wrap img{
+		width: 124upx;
+		height: 34upx;
+	}
+	
+	.brand_wrap span{
+		color: #000000;
+		font-size: 32upx;
+	}
+	
+	.price {
+	  color: #f00;
+	  font-size: 32upx;
+	  text-align: right;
+	  display: flex;
+	  flex-direction: column;
+	  
+	}
+	.price .name{
+		font-size: 28upx;
+		color: #BBBBBB;
+	  }
+	  
+	  .address_wrap{
+		  width: 100%;
+		  box-sizing: border-box;
+		  padding: 40upx;
+		  font-size: 30upx;
+		  color: #676767;
+	  }
+	  
+	  .title {
+		  border-left: #427DFF 10upx solid;
+		  padding: 0upx 30upx;
+		  font-size: 34upx;
+		  color: #3D3D3D;
+		  font-weight: 600;
+		  background-color: #F9F9F9;
+		  height: 70upx;
+		  line-height: 70upx;
+		  box-sizing: border-box;
+		}
+		
+		.change {
+  text-align: right;
+  color: #427DFF;
+  font-size: 28upx;
+}
+
+.content_wrap{
+	width: 100%;
+	box-sizing: border-box;
+	background-color: #FFFFFF;
+	padding: 10upx 40upx 30upx;
+	font-size: 30upx;
+	color: #676767;
+}
+
+.content_item{
+	margin-top: 10upx;
+}
+
+.content_item .tag{
+	font-size: 20upx;
+	color: #FF4242;
+	background-color: #FFF2EC;
+	border-radius: 6upx;
+	padding: 0upx 10upx;
+	margin-left: 32upx;
+}
+
+.license {
+  padding: 20upx 40upx 0upx;
+  border-top: 1upx solid #E4E4E4;
+}
+
+.link{
+  color: #427DFF;
+}
+
+.button{
+  margin: 40upx 5%;
+  width: 90%;
+  height: 94upx;
+  line-height: 94upx;
+  color: #ffffff;
+  background-color: #427DFF;
+  border-radius: 10upx;
+  text-align: center;
+  box-sizing: border-box;
+    
+}
+
+checkbox .wx-checkbox-input{
+   border-radius: 50%;/* 圆角 */
+   width: 30upx; /* 背景的宽 */
+   height: 30upx; /* 背景的高 */
+}
+/* 选中后的 背景样式 （红色背景 无边框 可根据UI需求自己修改） */
+checkbox .wx-checkbox-input.wx-checkbox-input-checked{
+   border: none;
+   background: #427DFF;
+   padding: 3upx;
+}
+/* 选中后的 对勾样式 （白色对勾 可根据UI需求自己修改） */
+checkbox .wx-checkbox-input.wx-checkbox-input-checked::before{
+   border-radius: 50%;/* 圆角 */
+   width: 30upx;/* 选中后对勾大小，不要超过背景的尺寸 */
+   height: 30upx;/* 选中后对勾大小，不要超过背景的尺寸 */
+   line-height: 30upx;
+   text-align: center;
+   font-size:20upx; /* 对勾大小 30upx */
+   color:#fff; /* 对勾颜色 白色 */
+   background: transparent;
+   transform:translate(-50%, -50%) scale(1);
+   -webkit-transform:translate(-50%, -50%) scale(1);
+}
+
+</style>
---
+++ lccx-web/pages/pay_one/pay_one.vue
@@ -0,0 +1,165 @@
+<template>
+<div class="container container-fill">
+
+  <div class="item">
+    <div class="item_left">
+      <img src="../../static/img/finish_gray.png" alt="">
+      <span></span>
+      <img src="../../static/img/finish_ready.png" alt="">
+    </div>
+
+    <div class="right">
+        <div class="item_right">
+          <div class="item_right_wrap">
+            <span class="name">里程车险</span>
+            <span class="money">服务费用：400元</span>
+          </div>
+          <span class="item_status">已付款</span>
+        </div>
+
+        <div class="item_right" style="margin-bottom: 0rpx;">
+          <div class="item_right_wrap">
+            <span class="name">交强险</span>
+            <span class="money">服务费用：970元</span>
+          </div>
+          <span class="item_status active">未付款</span>
+        </div>
+    </div>
+    
+
+  </div>
+
+
+
+  <button class="button" @click="next">
+    下一步
+  </button>
+
+</div>
+</template>
+<script>
+
+export default {
+  data(){
+    return{
+
+    }
+  },
+
+
+  methods: {
+    next () {
+      wx.navigateTo({
+        url: "../pay_all/main"
+      })
+    },
+  },
+
+  created () {
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  font-size: 28rpx;
+  background-color: #F9F9F9;
+  width: 100%;
+}
+
+.item{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 40rpx 50rpx 60rpx;
+  box-sizing: border-box;
+  width: 100%;
+  .item_left{
+    width: 80rpx;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20rpx;
+    height: 100%;
+    span{
+      height: 180rpx;
+      width: 2rpx;
+      background-color: #ECECEC;
+    }
+    img{
+      width: 80rpx;
+      height: 80rpx;
+    }
+  }
+  .right{
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+  .item_right{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    box-shadow: 0rpx 0rpx 20rpx 0rpx #EAEAEA;
+    border-radius: 12rpx;
+    padding: 52rpx 40rpx;
+    height: 220rpx;
+    box-sizing: border-box;
+    background-color: #FFFFFF;
+    justify-content: space-between;
+    margin-bottom: 40rpx;
+    .item_right_wrap{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .name{
+        color: rgba(77,77,77,0.4);
+        font-size: 34rpx;
+      }
+      .money{
+        font-size: 28rpx;
+        color: rgba(159,159,159,0.5);
+      }
+    }
+    .item_status{
+        background-color: rgba(0,0,0,0.1);
+        color: #FFFFFF;
+        border-radius: 26rpx;
+        width: 128rpx;
+        height: 52rpx;
+        line-height: 52rpx;
+        text-align: center;
+        align-self: center;
+      }
+    .active{
+      color: #3A7FF7;
+      background-color: #E9F3FB;
+    }  
+  }
+}
+
+.info{
+  text-align: center;
+  font-size: 28rpx;
+  opacity: 0.3;
+  color: #000000;
+}
+
+.button{
+  margin: 40upx 5%;
+  width: 90%;
+  height: 94upx;
+  line-height: 94upx;
+  color: #ffffff;
+  background-color: #427DFF;
+  border-radius: 10upx;
+  text-align: center;
+  box-sizing: border-box;
+  position: absolute;
+  bottom: 40upx;
+}
+
+</style>
