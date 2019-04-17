445eb0147db75423e40b8d36c3b40cc4be522fd5
guzhenfu <gzfgeh@sina.com>
2019-04-18 01:01:28 +0800

    update file

--- ball-mall/App.vue
+++ ball-mall/App.vue
@@ -7,8 +7,8 @@
 			let tmpParas= this.GetRequestParameters(tmpUrlSearch);
 			let openid = tmpParas["openid"];//提取参数
 			console.log(openid);
-			uni.setStorageSync("openid", openid);
-			// uni.setStorageSync("openid", "oms5B1cYgVWMCWtDOgiTzH6EUmp4");
+			// uni.setStorageSync("openid", openid);
+			uni.setStorageSync("openid", "oms5B1cYgVWMCWtDOgiTzH6EUmp4");
         },
         onShow: function() {
             console.log('App Show');
