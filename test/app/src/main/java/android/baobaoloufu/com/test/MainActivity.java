19d2b4d28faa132f7d0277e1992ba10af73d79cb
guzhenfu <867124018@mslife.com>
2019-06-13 18:04:27 +0800

    update file

---
+++ test/app/src/main/java/android/baobaoloufu/com/test/MainActivity.java
@@ -0,0 +1,172 @@
+package android.baobaoloufu.com.test;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.unionpay.UPPayAssistEx;
+import com.unionpay.mobile.android.plugin.BaseActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity implements Handler.Callback,
+        Runnable{
+
+    private static final String TN_URL_01 = "http://apitest.baobaoloufu.com/pay/index/yunshanfu_app";
+    private Handler mHandler = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHandler = new Handler(this);
+        TextView test = findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(MainActivity.this).start();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        String tn = "";
+        if (msg.obj == null || ((String) msg.obj).length() == 0) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("错误提示");
+            builder.setMessage("网络连接失败,请重试!");
+            builder.setNegativeButton("确定",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            builder.create().show();
+        } else {
+            tn = (String) msg.obj;
+            /*************************************************
+             * 步骤2：通过银联工具类启动支付插件
+             ************************************************/
+            UPPayAssistEx.startPay(this, null, null, tn, "01");
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        /*************************************************
+         * 步骤3：处理银联手机支付控件返回的支付结果
+         ************************************************/
+        if (data == null) {
+            return;
+        }
+
+        String msg = "";
+        /*
+         * 支付控件返回字符串:success、fail、cancel 分别代表支付成功，支付失败，支付取消
+         */
+        String str = data.getExtras().getString("pay_result");
+        if (str.equalsIgnoreCase("success")) {
+
+            // 如果想对结果数据验签，可使用下面这段代码，但建议不验签，直接去商户后台查询交易结果
+            // result_data结构见c）result_data参数说明
+            if (data.hasExtra("result_data")) {
+                String result = data.getExtras().getString("result_data");
+                try {
+                    JSONObject resultJson = new JSONObject(result);
+                    String sign = resultJson.getString("sign");
+                    String dataOrg = resultJson.getString("data");
+                    // 此处的verify建议送去商户后台做验签
+                    // 如要放在手机端验，则代码必须支持更新证书
+                    boolean ret = verify(dataOrg, sign, "01");
+                    if (ret) {
+                        // 验签成功，显示支付结果
+                        msg = "支付成功！";
+                    } else {
+                        // 验签失败
+                        msg = "支付失败！";
+                    }
+                } catch (JSONException e) {
+                }
+            }
+            // 结果result_data为成功时，去商户后台查询一下再展示成功
+            msg = "支付成功！";
+        } else if (str.equalsIgnoreCase("fail")) {
+            msg = "支付失败！";
+        } else if (str.equalsIgnoreCase("cancel")) {
+            msg = "用户取消了支付";
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("支付结果通知");
+        builder.setMessage(msg);
+        builder.setInverseBackgroundForced(true);
+        // builder.setCustomTitle();
+        builder.setNegativeButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    private boolean verify(String msg, String sign64, String mode) {
+        // 此处的verify，商户需送去商户后台做验签
+        return true;
+
+    }
+
+    @Override
+    public void run() {
+        String tn = null;
+        InputStream is;
+        try {
+
+            String url = TN_URL_01;
+
+            URL myURL = new URL(url);
+            URLConnection ucon = myURL.openConnection();
+            ucon.setConnectTimeout(120000);
+            is = ucon.getInputStream();
+            int i = -1;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            while ((i = is.read()) != -1) {
+                baos.write(i);
+            }
+
+            tn = baos.toString();
+            is.close();
+            baos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Message msg = mHandler.obtainMessage();
+
+        try {
+            JSONObject object = new JSONObject(tn);
+            msg.obj = object.getString("data");
+            mHandler.sendMessage(msg);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
