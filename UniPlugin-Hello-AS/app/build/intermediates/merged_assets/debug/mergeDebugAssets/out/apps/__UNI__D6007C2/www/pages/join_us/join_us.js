ca6ec55ec4ac87538d64905f1c5794aad857669a
guzhenfu <gzfgeh@sina.com>
2019-07-10 22:43:34 +0800

    add file

--- UniPlugin-Hello-AS/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/apps/__UNI__D6007C2/www/pages/join_us/join_us.js
+++
@@ -1,2 +0,0 @@
-setCssToHead([".",[1],"item_wrap{height: ",[0,300],"; width: 96%; margin: ",[0,10]," 2%;position: relative;color: #FFF; font-size: ",[0,32],";}\n.",[1],"item_wrap .",[1],"_img{position: absolute;top: ",[0,0],"; left: ",[0,0],"; width: 100%; height: 100%; z-index: -1;}\n",],undefined,{path:"./pages/join_us/join_us.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/join_us/join_us.wxml') } }));
